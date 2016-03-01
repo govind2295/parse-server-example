815c4eafcfdf53db764f8a3f79c74ef229be4271
govind2295 <govindanjajoo@gmail.com>
2016-03-01 11:32:58 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,21 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+var Pubnub = require('cloud/pubnub');
+Parse.Cloud.define('hello_world', function (request, response) {
+  var pubnub = PubNub({
+    publish_key: 'pub-c-d3caf25f-b2d2-457b-8a47-61e7769b83f7',
+    subscribe_key: 'sub-c-14066ce2-df6b-11e5-9f0d-02ee2ddab7fe'
+  });
+ 
+  pubnub.publish({
+    channel: "hello_world",
+    message: "Hello from PubNub Docs!",
+    callback: function (result) {
+      response.success(result);
+    },
+    error: function (error) {
+      response.error(error)
+    }
+  });
+});
